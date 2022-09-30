a73995de5b663ea2cce177a1273b4246f63607a8
Susie-Mars <marsnarsbars@gmail.com>
2022-09-29 20:42:49 -0600

    playground

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/play')
+def welcome_boxes():
+    return render_template("index.html")
+
+@app.route('/play/<int:num>')
+def welcome_boxes2(num):
+    return render_template("index1.html", num=num)
+
+@app.route('/play/<int:num>/<color>')
+def welcome_boxes1(num, color):
+    return render_template("index2.html", num=num, color=color)
+    
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
